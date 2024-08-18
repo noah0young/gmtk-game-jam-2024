a1116d02e534464d81873351f3ee83d031e31b10
RedMakuta <yc97748@gmail.com>
2024-08-17 23:45:36 -0400

    still no work >:(

--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -8,15 +8,16 @@ using Random = UnityEngine.Random;
 
 public class ShopManager : MonoBehaviour
 {
-    private int money;
 
     [SerializeField] public GameObject[] possibleComponents;
 
     [SerializeField] public CapsuleManager[] capsules;
+    
+    [SerializeField] public GameObject inventory;
 
-    [SerializeField] public Button[] buyButtons;
-
-    public GameObject Inventory;
+    [SerializeField] public GameObject screen;
+    
+    private int money = 100;
     
     // Start is called before the first frame update
     void Start()
@@ -57,26 +58,33 @@ public class ShopManager : MonoBehaviour
         capsules[2].Set(temp, temp.GetComponent<Image>().sprite, tempCost, tempSale);
     }
 
-    private GameObject randomComponent()
-    {
+    private GameObject randomComponent() {
         return possibleComponents[Random.Range(0, possibleComponents.Length)];
     }
 
-    private int getCostForComponent(GameObject component)
-    {
+    private int getCostForComponent(GameObject component) {
         int baseCost = component.GetComponent<Cost>().baseCost;
         int extra = Random.Range(-1, 2);
         return math.min(1, baseCost + extra);
     }
 
-    private bool isOnSale()
-    {
+    private bool isOnSale() {
         return Random.Range(0, 100) < 2;
     } 
     
     public void BuyItem(CapsuleManager capsule) {
-        
-        GameObject go = Instantiate(capsule.componentPrefab,Inventory.transform);
-        RefreshCapsules();
+        if (capsule.cost > money) {
+            // cannot buy
+            screen.transform.GetChild(0).gameObject.SetActive(false);
+            screen.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            // purchase successful
+            screen.transform.GetChild(1).gameObject.SetActive(true);
+            screen.transform.GetChild(1).gameObject.SetActive(false);
+            GameObject go = Instantiate(capsule.componentPrefab,inventory.transform);
+            RefreshCapsules();
+        }
     }
 }
